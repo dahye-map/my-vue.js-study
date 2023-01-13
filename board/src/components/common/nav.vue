05c5cb674837e59e86e9dfdb01a2c168f2d097fe
[dahye] <[dahae5919@gmail.com]>
2023-01-13 11:30:17 +0900

    nav 수정

--- board/src/components/common/nav.vue
+++ board/src/components/common/nav.vue
@@ -3,22 +3,22 @@
         <div class="nav">
             <ul>
                 <li><router-link to="/List">관리자 설정</router-link></li>
-                <li><a href="">권한 관리ㅇㅇ</a></li>
-                <li><a href="">안심전세 관리</a></li>
+                <li><router-link to="/List">권한 관리</router-link></li>
+                <li><router-link to="/List">안심전세 관리</router-link></li>
                 <li><a href="javascript:void(0)">법률상담 관리</a>
                     <ul class="depth-menu">
-                        <li><a href="">상담배정 내역조회</a></li>
-                        <li><a href="">법률상담 내역조회</a></li>
+                        <li><router-link to="/List">상담배정 내역조회</router-link></li>
+                        <li><router-link to="/List">법률상담 내역조회</router-link></li>
                     </ul>
 
                 </li>
-                <li><a href="">법률사례 조회</a></li>
-                <li><a href="">고객관리</a></li>
-                <li><a href="">공지사항 관리</a></li>
+                <li><router-link to="/List">법률사례 조회</router-link></li>
+                <li><router-link to="/List">고객관리</router-link></li>
+                <li><router-link to="/List">공지사항 관리</router-link></li>
                 <li><a href="javascript:void(0)">통계 관리</a>
                     <ul class="depth-menu">
-                        <li><a href="">접속통계 관리</a></li>
-                        <li><a href="">이용통계 관리</a></li>
+                        <li><router-link to="/List">접속통계 관리</router-link></li>
+                        <li><router-link to="/List">이용통계 관리</router-link></li>
                     </ul>
 
                 </li>
