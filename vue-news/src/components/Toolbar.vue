62235d19e072dbd3a8caf773229c05e7795196ea
dahye <dahae5919@gmail.com>
2023-06-19 23:01:17 +0900

    update : api response

---
+++ vue-news/src/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link>
+    <router-link to="/ask">Ask</router-link>
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .header {
+    color:#fff;
+    background-color:#42b883;
+    display: flex;
+    padding:8px;
+  }
+  .header .router-link-exact-active {
+    color:#35495e;
+  }
+  .header a {
+    color:#fff;
+    margin-right:20px;
+    font-size:24px;
+  }
+</style>
